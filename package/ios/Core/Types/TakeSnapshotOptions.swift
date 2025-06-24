352f72101e71a9da04a961a77cd7f8f5f29f30ee
bssn01 <eduardo.bassani01@gmail.com>
2025-06-24 17:10:23 -0300

    troca formato final da foto

--- package/ios/Core/Types/TakeSnapshotOptions.swift
+++ package/ios/Core/Types/TakeSnapshotOptions.swift
@@ -19,9 +19,9 @@ struct TakeSnapshotOptions {
     }
     // Custom Path
     if let customPath = dictionary["path"] as? String {
-      path = try FileUtils.getFilePath(customDirectory: customPath, fileExtension: "jpg")
+      path = try FileUtils.getFilePath(customDirectory: customPath, fileExtension: "png")
     } else {
-      path = try FileUtils.getFilePath(fileExtension: "jpg")
+      path = try FileUtils.getFilePath(fileExtension: "png")
     }
   }
 }
